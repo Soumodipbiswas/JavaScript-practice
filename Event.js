d0095155d7d1f3cff272dcb9251f75dcabfc0609
SOUMODIP BISWAS <soumodipbiswas80@gmail.com>
2026-02-27 10:14:30 +0530

    completed events

--- Event.js
+++ Event.js
@@ -5,3 +5,23 @@ btn1.onclick = () => {
     a++;
     console.log(a);
 }
+let div1 = document.querySelector("#div1");
+ div1.addEventListener("mouseover", (evt)=>{
+    console.log("You are inside div 1.");
+    console.log(evt.type);
+ });
+ div1.addEventListener("mouseover", (evt)=>{
+    console.log("You are inside div 2.");
+    console.log(evt.type);
+ });
+ const handler3 = (evt)=>{
+    console.log("You are inside div 3.");
+    console.log(evt.type);
+ };
+ div1.addEventListener("mouseover", handler3);
+ div1.addEventListener("mouseover", (evt)=>{
+    console.log("You are inside div 4.");
+    console.log(evt.type);
+ });
+
+ div1.removeEventListener("mouseover", handler3);
