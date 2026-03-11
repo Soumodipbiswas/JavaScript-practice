1b684c07485b5184a5e9cd1f68a332ea28f50a1c
SOUMODIP BISWAS <soumodipbiswas80@gmail.com>
2026-03-11 22:35:20 +0530

    add more animation to RPS website

--- RPS.js
+++ RPS.js
@@ -5,6 +5,54 @@ const choices = document.querySelectorAll(".choices");
 const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
+const userMoveToken = document.querySelector("#user-move");
+const compMoveToken = document.querySelector("#comp-move");
+
+const playOutcomeFx = (outcome) => {
+    const body = document.body;
+    if (!body) return;
+
+    body.classList.remove("state-win", "state-lose");
+    void body.offsetWidth; // restart CSS animation
+
+    if (outcome === "win") body.classList.add("state-win");
+    if (outcome === "lose") body.classList.add("state-lose");
+
+    window.clearTimeout(playOutcomeFx._t);
+    playOutcomeFx._t = window.setTimeout(() => {
+        body.classList.remove("state-win", "state-lose");
+    }, 1200);
+};
+
+const moveImages = {
+    rock: "./pictures/rock2.png",
+    paper: "./pictures/paper2.png",
+    scissors: "./pictures/67ebfd32e898b4ec3ae4e59ae33832c8.jpg",
+};
+
+const triggerMoveAnimation = () => {
+    [userMoveToken, compMoveToken].forEach((el) => {
+        if (!el) return;
+        el.classList.remove("animate-move");
+        // restart animation
+        void el.offsetWidth;
+        el.classList.add("animate-move");
+    });
+};
+
+const updateMoveDisplay = (userchoice, compchoice) => {
+    const userImg = moveImages[userchoice];
+    const compImg = moveImages[compchoice];
+
+    if (userMoveToken && userImg) {
+        userMoveToken.style.backgroundImage = `url('${userImg}')`;
+    }
+    if (compMoveToken && compImg) {
+        compMoveToken.style.backgroundImage = `url('${compImg}')`;
+    }
+
+    triggerMoveAnimation();
+};
 
 const showWinner = (userWin, userchoice, compchoice) => {
     if (userWin) {
@@ -13,12 +61,14 @@ const showWinner = (userWin, userchoice, compchoice) => {
         console.log("Congratulations! You win!");
         msg.innerText = `Congratulations! You win! Your ${userchoice} beats Computer's ${compchoice}`;
         msg.style.backgroundColor = "green";
+        playOutcomeFx("win");
     } else {
         compScore++;
         compScorePara.innerText = compScore;
         console.log("Oops! You lose! Computer wins!");
         msg.innerText = `Oops! You lose! Computer wins! Your ${userchoice} loses to Computer's ${compchoice}`;
         msg.style.backgroundColor = "red";
+        playOutcomeFx("lose");
     }
 }
 
@@ -32,10 +82,12 @@ const playGame = (userchoice) => {
     console.log("User choice : ", userchoice);
     const compchoice = genCompChoice();
     console.log("Computer choice : ", compchoice);
+    updateMoveDisplay(userchoice, compchoice);
     if (userchoice === compchoice) {
         console.log("It's a tie! Play again!");
         msg.innerText = "It's a tie! Play again!";
         msg.style.backgroundColor = "grey";
+        playOutcomeFx("tie");
     } else {
         let userWin = true;
         if(userchoice === "rock"){
